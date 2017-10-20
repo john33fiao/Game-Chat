df938abceb1a0eb71b6fa893f0eb771147a803d6
John Seo <john33fiao@gmail.com>
2017-10-20 17:04:24 +0900

    Java Study

--- day01/src/day01/Ex02.java
+++ day01/src/day01/Ex02.java
@@ -3,6 +3,18 @@ package day01;
 public class Ex02 {
 
 	public static void main(String[] args) {
+		System.out.println("한글");	// 한글 표현이 가능하다
+		System.out.println('가'+'a'-97);	// ''로 하면 하나의 문자가 선택, 숫자를 빼봐라
+		System.out.println("test");
+		System.out.println(1004);
+		int a; // 변수의 선언
+		a=100;
+		// 변수 사용시
+		// 변수의 선언 - 초기화
+		// 초기화하지 않을시 0이 기본값이지만, 초기화하는 것을 권장한다
+		int b=3;
+		System.out.println(a+b);
+		
 		
 	}
 
