240ee3170ecb8a857051efb111e21e52ef8acdbb
john33fiao <john33fiao@gmail.com>
2017-10-21 19:31:18 +0900

    Re-Study

--- day01/src/day01/Ex08.java
+++ day01/src/day01/Ex08.java
@@ -3,6 +3,11 @@ package day01;
 public class Ex08 {
 
 	public static void main(String [] args){
-		
+		for(int i=0; i<5; i++) {
+			for(int j=0; j<=i; j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
 	}
 }
